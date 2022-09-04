90b399e523c3a0f587474fcd3b4795c9aafab071
DakshSuryavanshi <dakshsuryavanshi2003@gmail.com>
2022-09-04 19:17:22 +0530

    test 01

--- app/src/main/java/com/example/calci/MainActivity.kt
+++ app/src/main/java/com/example/calci/MainActivity.kt
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var calctxt:TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        calctxt = findViewById(R.id.calctxt)
     }
+
     fun onDigit(view: View) {
-        Toast.makeText(this,"btn clicked",Toast.LENGTH_SHORT).show()
+        calctxt?.append("1")
     }
 
 
